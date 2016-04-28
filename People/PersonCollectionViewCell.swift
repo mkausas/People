feb2d790eeea103b8cfd707ee0b794cd3aaf564f
mkausas <mkausas@gmail.com>
2016-04-28 12:12:26 -0400

    Images for attendees now load in full

--- People/PersonCollectionViewCell.swift
+++ People/PersonCollectionViewCell.swift
@@ -24,6 +24,21 @@ class PersonCollectionViewCell: UICollectionViewCell {
 
             if let img = attendee?.profileImage {
                 profileImageView.image = img
+            } else {
+                let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
+                dispatch_async(dispatch_get_global_queue(priority, 0), {
+                    while (self.attendee?.profileImage == nil) {
+                        // still grabbing profile image
+                    }
+                    
+                    dispatch_async(dispatch_get_main_queue(), {
+                        self.alpha = 0
+                        self.profileImageView.image = self.attendee?.profileImage
+                        UIView.animateWithDuration(0.7, animations: {
+                            self.alpha = 1
+                        })
+                    })
+                })
             }
         }
     }
